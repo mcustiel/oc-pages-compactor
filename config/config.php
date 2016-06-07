d586ce06866ba933e7ec8ea559da6c577071ec1d
Mariano Custiel <mcustiel@goodgamestudios.com>
2016-06-07 09:51:37 +0200

    Fixed composer file

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
 return [
     'compactation' => [
         'enabled' => false,
-        'compactor' => '\\Mcustiel\\CompactPages\\Classes\\Services\\Implementation\\PeeWeeCompactor',
+        'compactor' => '\\Mcustiel\\CompactPages\\Classes\\Services\\Implementation\\PeeWeeHtmlCompactor',
     ],
 ];
